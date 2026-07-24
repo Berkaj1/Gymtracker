8737b3bc8b283bfae9679bd382bc309b126e0ae7
Berkay Simsek <berkaysimsek1@gmail.com>
2026-07-25 00:41:48 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,19 @@
-const CACHE = 'gymtracker-v1';
-const OFFLINE_FILES = [
-  '/Gymtracker/gym_tracker.html',
-];
+const CACHE = 'gymtracker-v3';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(OFFLINE_FILES))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
+// Network only - no caching, always fresh
 self.addEventListener('fetch', e => {
-  // Network first, fallback to cache
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
