8a556016366878b383910627103d6785ba2e4398
Berkay Simsek <berkaysimsek1@gmail.com>
2026-07-25 00:02:13 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'gymtracker-v1';
+const OFFLINE_FILES = [
+  '/Gymtracker/gym_tracker.html',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(OFFLINE_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Network first, fallback to cache
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
